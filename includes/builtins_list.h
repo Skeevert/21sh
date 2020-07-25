20771e7df9a27eac401f7b70c551fea4d0689839
Rickard Bednar <rbednar@mi-t3.msk.21-school.ru>
2020-07-25 14:33:49 +0300

    updagte

--- includes/builtins_list.h
+++ includes/builtins_list.h
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_list.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbednar <rbednar@student.21school.ru>      +#+  +:+       +#+        */
+/*   By: rbednar <rbednar@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 19:07:24 by rbednar           #+#    #+#             */
-/*   Updated: 2020/07/22 23:10:14 by rbednar          ###   ########.fr       */
+/*   Updated: 2020/07/25 14:32:38 by rbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_LIST_H
 # define BUILTINS_LIST_H
 
-# define BUILTINS_NUM 9
+# define BUILTINS_NUM 7
 
-static char		*g_builtins[] = {"setenv", "unsetenv", "env", "set", "unset", \
+static char		*g_builtins[] = {"setenv", "unsetenv", "env", \
 	"cd", "echo", "exit", NULL};
 
 static int 		(*g_builtins_func[BUILTINS_NUM])() = {btin_setenv, btin_unsetenv, btin_env, \
-	btin_set, btin_unset, btin_cd, btin_echo, btin_exit, NULL};
+	btin_cd, btin_echo, btin_exit, NULL};
 
 #endif
